84dff07f69832a61a20cbbdaccf9d91d2204c782
Lynn Adele Sadler <lynnadelesadler@yahoo.com>
2022-11-30 13:54:01 -0500

    fix Routes to routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const apiRoutes = require("./Routes/apiRoutes");
-const htmlRoutes = require("./Routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
